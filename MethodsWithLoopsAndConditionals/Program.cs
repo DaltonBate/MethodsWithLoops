96288a2e1846cc5a2e3d5c048ba7b5e6c6ee54b9
Dalton Bate <dilton8763@gmail.com>
2024-05-01 11:02:49 -0400

    Excersise three fix

--- MethodsWithLoopsAndConditionals/Program.cs
+++ MethodsWithLoopsAndConditionals/Program.cs
@@ -6,7 +6,7 @@ namespace MethodsWithLoopsAndConditionals
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
              Threes();
         }
--- MethodsWithLoopsAndConditionals/obj/Debug/net8.0/MethodsWithLoopsAndConditionals.AssemblyInfo.cs
+++ MethodsWithLoopsAndConditionals/obj/Debug/net8.0/MethodsWithLoopsAndConditionals.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsWithLoopsAndConditionals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e96546d603f955758199332cc770fc4af55d7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dd599078a6d51244eecd04ad602b1b09f1ea9c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsWithLoopsAndConditionals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsWithLoopsAndConditionals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
